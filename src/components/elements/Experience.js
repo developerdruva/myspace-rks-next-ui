075cead5ae900459a83b0952760696dd5057af3d
Rajesh <developer.rajeshnuz@gmail.com>
2025-11-18 13:19:27 +0530

    enhancing components

--- src/components/elements/Experience.js
+++ src/components/elements/Experience.js
@@ -6,12 +6,16 @@ import { useSelector } from "react-redux";
 import ProjectCard from "../cards/ProjectCard";
 import classes from "../styles/Experience.module.css";
 import { Chip } from "@mui/material";
+import { useThemeMode } from "@/global/ThemeProvider";
+import { useEffect } from "react";
 
 const Experience = () => {
   const portfolioDetails = useSelector((state) => state?.portfolioState);
-  const themeModeFrmRdx = useSelector(
-    (state) => state?.themeModeState?.themeMode
-  );
+  const theme = useThemeMode();
+  const isLight = theme.theme == "light" ? true : false;
+  useEffect(() => {
+    console.log("theme ", theme.theme, isLight);
+  }, [theme]);
 
   return (
     <div className={classes.box}>
@@ -20,37 +24,33 @@ const Experience = () => {
         {portfolioDetails?.workedCompanies?.map((comp, compIndex) => {
           return (
             <article className={`${classes.timeline_entry}`} key={compIndex}>
-              <div
-                className={`${classes.timeline_icon}`}
-                style={{ backgroundColor: colorToRGBA(comp?.color_code, 0.4) }}
-              >
+              <div className={`${classes.timeline_icon}`}>
                 <MdWork
                   style={{
-                    color: themeModeFrmRdx
-                      ? colorToRGBA(comp.color_code, 1)
-                      : "purple",
+                    color: isLight ? colorToRGBA(comp.color_code, 1) : "purple",
                   }}
                 />
               </div>
+
               <div
                 className={`${classes.mainCard}`}
                 style={{
-                  backgroundColor: themeModeFrmRdx ? "#f2f3f7" : "#1B2631",
-                  color: themeModeFrmRdx ? "black" : "lightblue",
+                  backgroundColor: isLight ? "#f2f3f7" : "#1B2631",
+                  color: isLight ? "black" : "lightblue",
                 }}
               >
                 <div className={classes.mainCardHeader}>
                   <div className={classes.companyDetails}>
                     <div
                       style={{
-                        color: themeModeFrmRdx ? comp.color_code : "steelblue",
+                        color: isLight ? comp.color_code : "steelblue",
                       }}
                     >
                       {comp?.company_name}{" "}
                     </div>
                     <div
                       style={{
-                        color: themeModeFrmRdx ? comp.color_code : "steelblue",
+                        color: isLight ? comp.color_code : "steelblue",
                         backgroundColor: colorToRGBA(comp.color_code, 0.2),
                       }}
                       className=""
@@ -59,15 +59,17 @@ const Experience = () => {
                     </div>
                   </div>
                   <Chip
-                    color={colorToRGBA(comp?.color_code, 0.2)}
-                  >
-                    {" "}
-                    <span style={{ color: "black" }}>
-                      {dateFormat(comp?.from_date, "MMM-YYYY") +
-                        " to " +
-                        dateFormat(comp?.to_date, "MMM-YYYY")}
-                    </span>
-                  </Chip>
+                    variant="filled"
+                    sx={{ backgroundColor: colorToRGBA(comp?.color_code, 0.2) }}
+                    label={
+                      <span>
+                        {" "}
+                        {dateFormat(comp?.from_date, "MMM-YYYY") +
+                          " to " +
+                          dateFormat(comp?.to_date, "MMM-YYYY")}
+                      </span>
+                    }
+                  />
                 </div>
 
                 <SideScrolling
